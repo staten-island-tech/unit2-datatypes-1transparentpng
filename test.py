3597450c758863a44856626a4004897f72d4fb11
1transparentpng <aidens151@nycstudents.net>
2025-09-18 09:18:07 -0400

    lots of truth tables

--- test.py
+++ test.py
@@ -1,25 +1,39 @@
 # test
 haveFriend = True
-haveMoney = False
+haveMoney = True
+isBanned = True
 def truthtabletest():
-    print("Have Friend? | Have Money? | Go Movies?")
-    print("-=" * 10)
-    if haveFriend and haveMoney:
-        print("True, True, True")
-    elif haveFriend == False and haveMoney == True:
-        print (f"")
+    print(" ")
+    print("Have Friend? | Have Money? | Is Banned? | Go Movies?")
+    print("-=" * 25)
+    if haveFriend or haveMoney:
+        if not isBanned:
+            print(f"{haveFriend}, {haveMoney}, {isBanned}, True [!1!]") # I have a friend OR the money to go to the theater
+        elif isBanned:
+            print(f"{haveFriend}, {haveMoney}, {isBanned}, False [!2!]") # I am banned from the theater
+        elif not haveFriend or haveMoney:
+            print(f"{haveFriend}, {haveMoney}, {isBanned}, False [!3!]") # I do not have a friend or the money to go to the theater
+        print("-=" * 25)
+        print(" ")
+truthtabletest()
+
+
+
+
+
 # boolean value
 movie = True
 Homework = True
 
-if Homework and movie == True:
-    print("I'd like to go to the movies, but I must do my homework!")
-elif Homework == True:
-    print("Doing homework!")
-elif movie == True:
-    print("Heading to the movies!")
-else:
-    print("Got nothing to do..")
+def yadayada():
+    if Homework and movie == True:
+        print("I'd like to go to the movies, but I must do my homework!")
+    elif Homework == True:
+        print("Doing homework!")
+    elif movie == True:
+        print("Heading to the movies!")
+    else:
+        print("Got nothing to do..")
 
 # integers and floats
 integer = 67 # whole numbers only
@@ -30,12 +44,13 @@ float = float(67.02) # numbers with decimals allowed
 
 # Challenge 1 (make a script that counts the words of a sentence)
 
-sentence = input("Type a sentence, and i'll give you the word count.")
-y = sentence.split( ) # Splits the string into a list of words. Since were splitting all other letters from a space, it essentially creates a list of words.
-wrdcount = 0
-for i in y: # For every word in the list, the variable wrdcount increases by 1 from 0.
-    wrdcount = wrdcount + 1 
-print(f"There are {wrdcount} words in that!") # prints the total amount of words :D
+def c1():
+    sentence = input("Type a sentence, and i'll give you the word count.")
+    y = sentence.split( ) # Splits the string into a list of words. Since were splitting all other letters from a space, it essentially creates a list of words.
+    wrdcount = 0
+    for i in y: # For every word in the list, the variable wrdcount increases by 1 from 0.
+        wrdcount = wrdcount + 1 
+    print(f"There are {wrdcount} words in that!") # prints the total amount of words :D
 
 
 # Challenge 2
